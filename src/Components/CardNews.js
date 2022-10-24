30cdf07ccc5d37451aaf5402c1db8d9b552d391c
Bruno Gusmão <brunogusmao43@gmail.com>
2022-10-24 15:14:31 -0300

    construindo as props e style dos componentes

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -16,19 +16,31 @@ class Cardnews extends HTMLElement {
         cardLeft.setAttribute('class','card__left');
 
         const autor = document.createElement('span');
+        autor.textContent = 'By ' + (this.getAttribute('autor') || 'annoymous');
         cardLeft.appendChild('autor', 'card__left');
 
         const linkTitulo = document.createElement('a');
+        linkTitulo.textContent = this.getAttribute('title');
+        linkTitulo.href = this.getAttribute('link-url');
         cardLeft.appendChild('linkTitulo', 'card__left');
 
         const newsConteudo = document.createElement('p');
+        newsConteudo.textContent = this.getAttribute('content');
         cardLeft.appendChild('newsConteudo', 'card__left');
 
-        const newsImagem = document.createElement('img');
-        cardLeft.appendChild('newsImagem', 'card__left');
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitulo);
+        cardLeft.appendChild(newsConteudo);
+
+
 
         const cardRight = document.createElement('div');
         cardLeft.setAttribute('class','card__right');
+
+        const newsImagem = document.createElement('img');
+        newsImagem.src = 'assets/images/asset-vader.png';
+        newsImagem.alt = 'foto da noticia';
+        cardLeft.appendChild('newsImagem', 'card__left');
 
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
@@ -38,6 +50,44 @@ class Cardnews extends HTMLElement {
 
     styles() {
         //estilizar nossos componentes
+        const style = document.createElement('style')
+        style.textContent = `
+            .card{
+                width:80%;
+                border: 1px solid black;
+                box-shadow: 12px 6px 86px 23px rgba(0,0,0,0.67);
+                -webkit-box-shadow: 12px 6px 86px 23px rgba(0,0,0,0.67);
+                -moz-box-shadow: 12px 6px 86px 23px rgba(0,0,0,0.67);
+                display: flex;
+                flex-direction: row;
+                justify-content:space-between;
+            }
+            
+            .card__left, .card__right {
+                border: 1px solid blue;
+            }
+            
+            .card__left{
+                display: flex;
+                flex-direction: column;
+                justify-content:center;
+                padding-left:20px;
+            }
+            
+            .card__left > h1 {
+                margin-top: 18px;
+                font-size: 26px;
+            }
+            
+            .card__left > p {
+                color: rgb(95, 95, 95);
+            }
+            
+            .card__left > span {
+                font-weight: 430;
+            }
+        `
+        return style;
     }
 }
 
