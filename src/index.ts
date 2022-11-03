962f5a7d74b53df9abb351eec6b77a64991ac6f6
Bruno Gusmão <brunogusmao43@gmail.com>
2022-11-03 14:16:07 -0300

    funções e interfaces

--- src/index.ts
+++ src/index.ts
@@ -60,3 +60,54 @@ dados.map
 dados.push
 dados.forEach
 
+//funções
+
+function addNumber(x:number, y:number): number {
+    return x + y;
+}
+
+//função que aceita mais de um tipo ao mesmo tempo
+
+function CallToPhone(phone: number | string): any {
+    return phone;
+}
+
+console.log(CallToPhone("ligando"));
+
+//funções assincronas 
+
+async function getDatabase(id: number): Promise<string> {
+    return "bruno";
+}
+
+
+
+let soma: number = addNumber(5,5);
+
+console.log(soma);
+
+//interfaces (type x interface)
+
+type robot = {
+    id: number;
+    name: string;
+};
+
+interface robot2 {
+    id: number;
+    name: string;
+}
+
+const bot1: robot = {
+    id:1,
+    name:"megabot",
+};
+
+const bot2: robot2 = {
+    id:1,
+    name:"megabot",
+};
+
+console.log(bot1);
+console.log(bot2);
+
