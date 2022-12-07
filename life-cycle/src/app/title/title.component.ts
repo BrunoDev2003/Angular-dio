0942b1451a96f5e38faebbde11c14dcb96f7e223
Bruno Gusmão <brunogusmao43@gmail.com>
2022-12-07 14:26:42 -0300

    OnInit, OnChanges

--- life-cycle/src/app/title/title.component.ts
+++ life-cycle/src/app/title/title.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit, OnChanges {
+
+  nome:string = 'bruno';
+  @Input() idade:number = 19;
+
+  constructor() { 
+
+    console.log('construtor')
+  }
+  ngOnChanges(): void {
+    console.log('OnChange');
+  }
+  ngOnInit(): void {
+    console.log("ola eu sou o metodo ativado pelo construtor")
+    this.nome = `ola ${this.nome}`
+  }
 
 }
