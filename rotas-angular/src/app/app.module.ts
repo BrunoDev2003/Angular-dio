2dd98322b8a4f1cead200f0dfd624ff88454462b
Bruno Gusmão <brunogusmao43@gmail.com>
2022-12-21 14:57:55 -0300

    adicionando rotas

--- rotas-angular/src/app/app.module.ts
+++ rotas-angular/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {IndexModule} from './pages/index/index.module';
+import {PortfolioModule} from './pages/portfolio/portfolio.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IndexModule,
+    PortfolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
